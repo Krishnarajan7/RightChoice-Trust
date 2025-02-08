ffacd1c9dcdea2c5d17b9e6eb58fc269ebcd41c0
krishnarajan <krishh.v777@gmail.com>
2025-02-08 23:23:29 +0530

    js file updated

--- frontend/js/hamb.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  document.querySelector('.hamburger-menu').classList.toggle('active');
-  document.querySelector('.navitems').classList.toggle('active');
-}
-function toggleDropdown(event) {
-  event.preventDefault();
-  let dropdown = event.target.closest('.dropdown');
-  dropdown.classList.toggle('active');
-}
