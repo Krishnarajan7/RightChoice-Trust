1862496e10abefb4fdd99370637cdcd92d66fb0e
krishnarajan <krishh.v777@gmail.com>
2025-02-08 23:20:17 +0530

    chnages made

--- frontend/js/hamb.js
+++ frontend/js/hamb.js
@@ -1,4 +1,9 @@
 function toggleMenu() {
-  const navItems = document.querySelector('.navitems');
-  navItems.classList.toggle('show');
+  document.querySelector('.hamburger-menu').classList.toggle('active');
+  document.querySelector('.navitems').classList.toggle('active');
+}
+function toggleDropdown(event) {
+  event.preventDefault();
+  let dropdown = event.target.closest('.dropdown');
+  dropdown.classList.toggle('active');
 }
