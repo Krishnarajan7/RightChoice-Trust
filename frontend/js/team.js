1be72558f52caffa676afefd10473dc3f24fa807
krishnarajan <krishh.v777@gmail.com>
2025-03-14 22:58:59 +0530

    update

--- frontend/js/team.js
+++ frontend/js/team.js
@@ -2,7 +2,7 @@
 const teamMembers = [
   {
     name: 'L.Thanasekar',
-    degree: 'B.E,M.E - Mechanical Engineer',
+    degree: 'B.E,M.E - (Ph.D)',
     position: 'CEO',
     image: '/frontend/public/images/thanasekar_rm.png',
     description: 'Tenkasi'
